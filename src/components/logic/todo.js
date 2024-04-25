71b6436c3ada10c93559abb977f21b0ce0b254cb
Erne1984 <ernestofamorim@gmail.com>
2024-04-25 11:25:56 -0300

    some boilerplate and brainstorm

---
+++ src/components/logic/todo.js
@@ -0,0 +1,6 @@
+export default function ToDo(title, description, dueDate, priority){
+    this.title = title
+    this.description = description
+    this.dueDate = dueDate
+    this.priority = priority
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import ToDo from "./components/logic/todo";
 
+let newTodo = new ToDo("Louça", "Lavar a Louça", new Date(24, 12), "alta")
 
-console.log("teste");
+console.log(newTodo);
